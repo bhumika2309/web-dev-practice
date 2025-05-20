99cf90dda249696738da7d1aafada0cc375cf8c3
bhumika <bhumikasahu0923@gmail.com>
2025-05-20 09:24:56 +0530

    added new files

---
+++ backend/math.js
@@ -0,0 +1,12 @@
+const sum = (a,b) => (a+b);
+const mul = (a,b) => (a*b);
+const pi=3.14;
+const g=9.8;
+
+module.exports={
+    sum: sum,
+    mul:  mul,
+    pi: pi,
+    g: g
+
+}
---
+++ backend/script.js
@@ -0,0 +1,23 @@
+// let n = 5;
+// for(let i =0; i<=5;i++){
+//     console.log("hello"+n);
+// }
+// console.log("bye");
+
+
+// let args = process.argv;
+
+// for(let i=2;i<args.length;i++){
+//     console.log("hello, welcome ",args[i]);
+// }
+
+// let math= require("./math");
+
+// console.log(math.sum(2,2));
+// console.log(math.mul(2,4));
+
+const info = require("./fruits");
+
+console.log(info);
+  
+
